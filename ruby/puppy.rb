0c8c22514d0dd4f6c0d729a60bac8cd9e3cfc144
mattbehrens <mattbehrens96@gmail.com>
2016-06-28 23:36:01 -0500

    finish intro to classes

---
+++ ruby/puppy.rb
@@ -0,0 +1,13 @@
+#SPECIES ------------------------
+#German Shepard
+
+#CHARACTERISTICS ----------------
+#Hair Length: Short
+#Friendly: yes
+#Name: Max
+#Color: Black and Brown
+
+#BEHAVIOR -----------------------
+#Jump
+#Sit
+#Play Dead
